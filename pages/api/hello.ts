1c98530ec9345f723fc16c808265f66155398cc2
hsiaofongw <hsiaofong.w@gmail.com>
2021-03-08 08:28:51 +0800

    update

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,9 +2,10 @@
 
 import type { NextApiRequest, NextApiResponse } from 'next'
 import jwt, { VerifyOptions } from 'jsonwebtoken';
-import { VerifyErrors } from 'jsonwebtoken';
+import { VerifyErrors, VerifyCallback } from 'jsonwebtoken';
 import { v4 as uuidv4, v4 } from 'uuid';
-import { MongoClient } from 'mongodb';
+import { Db, MongoClient } from 'mongodb';
+import { promisify } from 'util';
 
 const masterSecret = "shhh";
 const mongoDBConnectionString = "mongodb+srv://online:<password>@cluster0.ky1q0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -94,9 +95,10 @@ class Visitor implements IVisitor {
         this.sessionHistories = [];
     }
 
-    createNewSession() {
+    createNewSession(): string {
         const session = new Session(this.visitorId);
         this.currentSessions.push(session);
+        return session.sessionId;
     }
 
     extendSessionLife(sessionId: string): boolean {
@@ -147,98 +149,242 @@ class Visitor implements IVisitor {
 
 }
 
+/**
+ * 返回给客户端内容的格式的约定．
+ */
 interface IResponseData {
+    jwt: string;
+    msg?: string;
+    renewed?: boolean;
+}
+
+interface IRequestBody {
     jwt: string;
 }
 
+class RequestBody implements IRequestBody {
+    public jwt: string;
+
+    constructor({ jwt }: IRequestBody) {
+        this.jwt = jwt;
+    }
+}
+
+/**
+ * 服务器和客户端以 JWT 的方式通信，这是 payload 所应满足的最基本要求．
+ */
 interface IPayload {
     visitorId: string;
     sessionId: string;
 }
 
-async function findVisitor(visitorId: string): Promise<Visitor | undefined>  {
-    const connStr = mongoDBConnectionString.replace("<password>", getDBPassword());
-    const client = new MongoClient(connStr, { useUnifiedTopology: true });
+class Payload implements IPayload {
+    public visitorId: string;
+    public sessionId: string;
 
-    try {
-        // Connect the client to the server
-        await client.connect();
-        // Establish and verify connection
-        const database = client.db("online");
-        const visitors = database.collection("visitors");
-        
-        const query = {
-            visitorId
-        };
-
-        const visitor = await visitors.findOne(query);
-
-        if (visitor) {
-            return Visitor.fromObject(visitor);
-        }
-    }
-    finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+    constructor({ visitorId, sessionId }: IPayload) {
+        this.visitorId = visitorId;
+        this.sessionId = sessionId;
     }
 }
 
-async function addVisitor(visitor: Visitor): Promise<boolean | undefined>  {
+async function getDatabase(): Promise<Db> {
     const connStr = mongoDBConnectionString.replace("<password>", getDBPassword());
     const client = new MongoClient(connStr, { useUnifiedTopology: true });
+    const dbName = "online";
 
     try {
-        // Connect the client to the server
         await client.connect();
-        // Establish and verify connection
+
         const database = client.db("online");
-        const visitors = database.collection("visitors");
-        
-        const data = visitor.toObject();
-        const result = await visitors.insertOne(data);
-
-        console.log(`${result.insertedCount} visitor inserted, insertedId: ${result.insertedId}, visitorId: ${visitor.visitorId}`);
-
-        if (result.insertedCount) {
-            return true;
-        }
+        return database;
     }
     finally {
         await client.close();
+    }
+    
+}
+
+/**
+ * 为新访客注册一个身份(Visitor)，并且返回对应的 jwt ．
+ */
+async function assignNewId(): Promise<string> {
+    let visitor = new Visitor;
+
+    const visitorId = visitor.visitorId;
+    const sessionId = visitor.createNewSession();
+
+    const payload: IPayload = {
+        visitorId, sessionId
+    };
+
+    const newJWTString = jwt.sign(
+        payload, masterSecret
+    );
+
+    return await getDatabase()
+    .then(db => db.collection("visitors"))
+    .then(collection => collection.insertOne(visitor))
+    .then(d => {
+        if (d.insertedCount) {
+            return newJWTString;
+        }
+        else {
+            return Promise.reject("Can't persist visitor into DB.")
+        }
+    });
+}
+
+/**
+ * 查找访客 Visitor 信息
+ * @param visitorId Visitor 的唯一标识符 
+ */
+async function findVisitor(visitorId: string): Promise<Visitor | undefined> {
+    const visitor = await getDatabase()
+    .then(db => db.collection("visitors"))
+    .then(collection => collection.findOne<Visitor>({ visitorId }));
+
+    if (visitor instanceof Visitor) {
+        return visitor;
+    }
+}
+
+/**
+ * 延迟会话时长
+ */
+async function extendLife(visitor: Visitor, sessionId: string) {
+
+    visitor.expireAllOutdatedSessions();
+    const result = visitor.extendSessionLife(sessionId);
+
+    return result
+}
+
+/**
+ * 使更改与数据库中的数据同步
+ * @param visitor 访客
+ */
+async function updateVisitor(visitor: Visitor) {
+    const query = {
+        visitorId: visitor.visitorId
+    };
+
+    const result = await getDatabase()
+    .then(db => db.collection("visitors"))
+    .then(collection => collection.findOneAndReplace(
+        query, visitor
+    ));
+
+    return result.ok;
+}
+
+async function dealWithInvalidJWT(req: NextApiRequest, res: NextApiResponse<IResponseData>) {
+    console.log(req);
+    console.log("Invalid JWT.");
+    const jwtStringPerhaps = await assignNewId();
+
+    if (jwtStringPerhaps) {
+        res.status(200).json({ jwt: `${jwtStringPerhaps}`});
+    }
+    else {
+        res.status(500).json({ jwt: "", msg: "Internal Error" });
     }
 }
 
 async function requestHandler(req: NextApiRequest, res: NextApiResponse<IResponseData>) {
 
+    if (req.body instanceof RequestBody) {
+        const body = req.body;
 
-    let v1 = new Visitor();
-    let v2 = new Visitor();
-    v1.createNewSession();
-    v1.createNewSession()
-    v2.createNewSession();
-    v2.createNewSession();
-    v2.createNewSession();
-    await addVisitor(v1);
-    await addVisitor(v2);
+        const verifyOptions: VerifyOptions = {
+            ignoreExpiration: true,
+            ignoreNotBefore: true
+        };
 
-    let d1 = await findVisitor(v1.visitorId);
-    let d2 = await findVisitor(v2.visitorId);
+        const verify = promisify(
+            (cb: VerifyCallback) => jwt.verify(body.jwt, masterSecret, verifyOptions, cb)
+        );
 
-    console.log(d1);
-    console.log(d2);
+        verify().then(d => {
+            if (d instanceof Payload) {
+                return Promise.resolve(d);
+            }
+            else {
+                return Promise.reject();
+            }
+        }).then(d => Promise.all([findVisitor(d.visitorId), d.sessionId]))
+        .then(vs => {
+            if (vs[0] instanceof Visitor) {
+                return Promise.all([vs[0], vs[1]])
+            }
+            else {
+                return Promise.reject();
+            }
+        }).then(vs => {
+            let visitor = vs[0];
+            let sessionId = vs[1];
 
-    res.status(200).json({ jwt: 'John Doe' })
+            visitor.expireAllOutdatedSessions();
+            const result = visitor.extendSessionLife(sessionId);
 
+            if (result) {
+                return visitor;
+            }
+            else {
+                return Promise.reject();
+            }
+        }).then(v => addVisitor(v))
+    }
+
+
+
+    if ("jwt" in body) {
+        const jwtString = body.jwt;
+
+        
+
+        let verifyCallback = async (err: VerifyErrors | null, decoded: object | undefined) => {
+            if (decoded instanceof Payload) {
+                const visitorId = decoded.visitorId;
+                const sessionId = decoded.sessionId;
+
+                const visitor = await findVisitor(visitorId);
+                if (visitor instanceof Visitor) {
+                    visitor.expireAllOutdatedSessions();
+                    const result = visitor.extendSessionLife(sessionId);
+
+                    if (result) {
+                        res.status(200).json({
+                            "jwt": jwtString,
+                            renewed: true
+                        });
+                    }
+                }
+            }
+        };
+
+        
+
+        verify().then(x => {
+            if (x instanceof Payload) {
+                return [findVisitor(x.visitorId), x.sessionId]
+            }
+            else {
+                return Promise.reject()
+            }
+        }).then(d => {
+
+        });
+
+
+    }
 }
 
 export default requestHandler;
 
     // const body = req.body;
 
-    // const verifyOptions: VerifyOptions = {
-    //     ignoreExpiration: true,
-    //     ignoreNotBefore: true
-    // };
+    
 
     // jwt.verify(
     //     body.jwt, 
